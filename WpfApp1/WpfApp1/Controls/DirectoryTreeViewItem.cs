2a59dca536369bd060229667c10d36b6be985bc0
eunju-lee-991 <eunju7757@gmail.com>
2020-07-18 19:16:22 +0900

    1cha success

---
+++ WpfApp1/WpfApp1/Controls/DirectoryTreeViewItem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1.Controls
+{
+    class DirectoryTreeViewItem : TreeViewItem
+    {
+        private DirectoryInfo MyDirectoryInfo;
+
+        private DirectoryTreeViewItem()
+        {
+
+        }
+
+        public DirectoryTreeViewItem(DirectoryInfo di)
+        {
+            Header = di.Name;
+            MyDirectoryInfo = di;
+
+            List<DirectoryInfo> list2 = new List<DirectoryInfo>();
+
+            foreach (DirectoryInfo dirInfo in di.GetDirectories())
+            {
+                list2.Add(dirInfo);
+            }
+            this.ItemsSource = list2;
+        }
+
+        protected override void OnExpanded(RoutedEventArgs e)
+        {
+            List< DirectoryTreeViewItem> list3 = new List<DirectoryTreeViewItem>();
+
+            foreach (DirectoryInfo dir in MyDirectoryInfo.GetDirectories())
+            {
+                list3.Add(new DirectoryTreeViewItem(dir));
+            }
+            this.ItemsSource = list3;
+        }
+
+        public DirectoryInfo GetDirectoryInfo()
+        {
+            return MyDirectoryInfo;
+        }
+    }
+}
--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp1.Controls;
 
 namespace WpfApp1
 {
@@ -23,6 +25,22 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            DirectoryInfo root = new DirectoryInfo("C:\\Users\\이경민\\Documents\\카카오톡 받은 파일");
+
+            List<DirectoryTreeViewItem> list = new List<DirectoryTreeViewItem>();
+
+            foreach (DirectoryInfo directoryInfo in root.GetDirectories())
+            {
+                list.Add(new DirectoryTreeViewItem(directoryInfo));
+            }
+
+            MyTreeView.ItemsSource = list;
         }
     }
 }
