e9629b24353d46cb05ed82c3abfd996ec761a989
eunju-lee-991 <eunju7757@gmail.com>
2020-07-20 20:58:35 +0900

    commit!

---
+++ WpfApp1/ChartTest/Controls/PointListBoxItem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ChartTest.Controls
+{
+    class PointListBoxItem : ListBoxItem
+    {
+        private char _XY;
+        public char XY
+        {
+            get => _XY;
+            set
+            {
+                _XY = value;
+            }
+        }
+
+        private FileInfo _MyFileInfo;
+        public FileInfo MyFileInfo
+        {
+            get => _MyFileInfo;
+            set
+            {
+                if(_MyFileInfo != value)
+                {
+                    _MyFileInfo = value;
+                }
+            }
+        }
+
+        public PointListBoxItem()
+        {
+
+        }
+
+        public PointListBoxItem(FileInfo datafile)
+        {            
+            MyFileInfo = datafile;
+            int idx = datafile.Name.IndexOf("_");
+            _XY = datafile.Name[idx + 1];
+            idx = datafile.Name.IndexOf("(");
+            Content = datafile.Name.Substring(idx + 1);
+        }
+    }
+}
---
+++ WpfApp1/ChartTest/ViewModel/MainViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using ChartTest.Controls;
+using OxyPlot;
+
+namespace ChartTest.ViewModel
+{
+    class MainViewModel
+    {
+        public MainViewModel()
+        {
+            this.Title = "ReadFileExample";
+
+            DirectoryInfo DataDirectory = new DirectoryInfo("Data");
+            if (!DataDirectory.Exists)
+            {
+                DataDirectory.Create();
+            }
+
+            FileInfo[] DataFiles = DataDirectory.GetFiles();
+
+            /*            List<PointListBoxItem> list = new List<PointListBoxItem>();
+
+                        foreach (FileInfo DataFile in DataFiles)
+                        {               
+                            list.Add(new PointListBoxItem(DataFile));
+                        }
+                        this.Datas = list;*/
+
+            List<List<PointListBoxItem>> list = new List<List<PointListBoxItem>>();
+
+            for(int i=0; i<DataFiles.Length; i++)
+            {
+                List<PointListBoxItem> pi = new List<PointListBoxItem>();
+                pi.Add(new PointListBoxItem(DataFiles[i]));
+
+                if (i%2 != 0)
+                {
+                    list.Add(pi);
+                    pi = null;
+                }
+            }
+
+            List<PointListBoxItem> result = new List<PointListBoxItem>();
+
+           for (int i=0; i<list.Count; i++)
+            {
+                result.Add(list[i][0]);
+            }
+            Datas = result;
+        }
+
+            
+        public string Title { get; private set; }
+        public IList<DataPoint> Points { get; private set; }
+        public List<PointListBoxItem> Datas { get; private set; }
+
+    }
+}
